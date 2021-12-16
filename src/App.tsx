8fabd6da24f138099317d7e25fdeaf1571611591
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-16 11:50:21 -1000

    added citysearch to add display

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
+import CitySearch from './components/CitySearch/CitySearch';
 function App() {
   return (
-    <div className="App">    
+    <div className="App"> 
+       <CitySearch />
       </div>
   );
 }
