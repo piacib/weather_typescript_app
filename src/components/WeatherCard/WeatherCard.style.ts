7a15ef6f9a202a637f01c255b61426f23b9e4015
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-16 11:54:25 -1000

    added styles for components for weather display

--- src/components/WeatherCard/WeatherCard.style.ts
+++ src/components/WeatherCard/WeatherCard.style.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
 export const WeatherCardContainer = styled.div``
+export const Date = styled.div``
+export const WeekDate = styled.div``
+export const WindSpeed = styled.div``
+export const Rain = styled.div``
+export const Humidity = styled.div``
+export const Snow = styled.div``
+export const Temperature = styled.div``
+export const Pressure = styled.div``
+export const WeatherDescription = styled.div``
