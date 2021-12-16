1c535ab323bde6117a19b3d1a7f00df1c3d1596e
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-16 11:55:41 -1000

    add styles for weather data

---
+++ src/components/WeatherCard/WeatherData.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const WeatherDataContainer= styled.div``
+export const Rain = styled.div``
+export const Humidity = styled.div``
+export const Snow = styled.div``
+export const Temperature = styled.div``
+export const Pressure = styled.div``
