c7684ec027668165151ddc053b7e99892b04ba91
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-16 08:24:15 -1000

    implemented useGeolocation hook and input for city search

--- src/components/CitySearch/CitySearch.tsx
+++ src/components/CitySearch/CitySearch.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import useGeoLocation from '../../hooks/useGeolocation'
 import {
-    CitySearchContainer
+    CitySearchContainer,
+    CitySearchInput
 } from './CitySearch.style'
+import magnifyingGlass from './magnifying-glass.svg'
 const CitySearch = () => {
+    const {location, } = useGeoLocation()
+    const {lat, lng} = location.coordinates
+    
+    const defaultPlaceHolder = 'Search your city';
+    const locationFoundPlaceHolder = 'Press search to use your location';
+
     return (
         <CitySearchContainer>
-            
+            <img src={magnifyingGlass} alt='Magnifying glass' />
+            <CitySearchInput placeholder={(!lat && !lng) ? defaultPlaceHolder: locationFoundPlaceHolder} type='text'/>
         </CitySearchContainer>
     )
 }
