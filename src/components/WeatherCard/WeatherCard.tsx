cebd256cf8d773e439f25cc8a8a272a7b62b3dc2
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-16 11:54:11 -1000

    added components for weather display

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,11 +1,54 @@
 import React from 'react'
 import {
-    WeatherCardContainer
+    WeatherCardContainer,
+    Date,
+    WeekDate,
+    WindSpeed,
+    Rain,
+    Humidity,
+    Snow,
+    Temperature,
+    Pressure,
+    WeatherDescription
 } from './WeatherCard.style'
-const WeatherCard = () => {
+interface WeatherDisplayProps {
+    dayOfTheWeek: string;
+    todaysDate: string;
+    windSpeed:  number;
+    rain: number;
+    humidity: number;
+    snow: number;
+    temperature: number;
+    pressure:number;
+    weatherDescription: string;
+}
+
+const WeatherCard:React.FC<WeatherDisplayProps> = ({
+    dayOfTheWeek,
+    todaysDate,
+    windSpeed,
+    rain,
+    humidity,
+    snow,
+    pressure,
+    temperature,
+    weatherDescription 
+}) => {
     return (
         <WeatherCardContainer>
-            
+            <Date>{dayOfTheWeek}</Date>
+            <WeekDate>{todaysDate}</WeekDate>
+            <WindSpeed>{windSpeed}</WindSpeed>
+            <WeatherData>
+            <Rain>{rain}</Rain>
+                <Humidity>{humidity}</Humidity>
+                <Snow>{snow}</Snow>
+                <Pressure>{pressure}</Pressure>
+            </WeatherData>
+            <Temperature>{temperature}</Temperature>
+            <WeatherDescription>{weatherDescription}</WeatherDescription>
+
+
         </WeatherCardContainer>
     )
 }
