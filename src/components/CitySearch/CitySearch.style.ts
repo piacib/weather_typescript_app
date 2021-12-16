01f41e2b1635765da9300f3dbe8ca027e353488e
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-16 08:25:00 -1000

    created styles for citysearch container and input

--- src/components/CitySearch/CitySearch.style.ts
+++ src/components/CitySearch/CitySearch.style.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const CitySearchContainer = styled.div``
+export const CitySearchContainer = styled.div`
+    background: white;
+    border-radius: 40px;
+    margin: 0 auto;
+    padding: 10px;
+    max-width: 800px;
+    display:flex;
+    align-items: center;
+    width:100%;
+`
+export const CitySearchInput = styled.input`
+    border:none;
+    font-size: 2rem;
+`
