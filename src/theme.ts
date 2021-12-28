6eec7a25303ad4364bd0ba80f9e74645fcae18bc
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-28 07:51:47 -1000

    added svg size

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,6 @@ import { DefaultTheme } from "styled-components";
 const theme: DefaultTheme = {
     backgroundColor:'linear-gradient(180deg, #62B8F6 0%, #2C79C1 77.96%)',
     fontColor: 'white',
+    dataSvgSize: 32,
 }
 export default theme
