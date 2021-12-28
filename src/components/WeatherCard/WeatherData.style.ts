f1df83f8c5d5c744261f789cb6d6f2cf433261c8
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-28 07:53:42 -1000

    styled components

--- src/components/WeatherCard/WeatherData.style.ts
+++ src/components/WeatherCard/WeatherData.style.ts
@@ -1,18 +1,17 @@
 import styled from "styled-components";
 export const WeatherDataContainer= styled.div`
-display: grid;
-grid-template-columns: repeat(3, 1fr 2fr) ;
-
-border-top: 1px solid ${props => props.theme.fontColor};
+    display: grid;
+    grid-template-columns: 140px 140px;
+    grid-row-gap: 15px;
+    max-width: 400px;
+    border-top: 1px solid ${props => props.theme.fontColor};
 `
-export const Rain = styled.div``
-export const Humidity = styled.div``
-export const Snow = styled.div``
-export const Temperature = styled.div``
-export const Pressure = styled.div`
-grid-column: 2;
+export const DataDisplayContainer = styled.div`
+    display: grid;
+    grid-template-columns: ${props => props.theme.dataSvgSize}px auto;
+    height: ${props => props.theme.dataSvgSize}px;
+    font-size: .666rem ;
 `
-export const WindSpeed = styled.div`
-grid-column: 2;
+export const DataTextContainer = styled.div`
 `
-
+export const DataNumberDisplay = styled.div``
