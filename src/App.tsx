5c7dc0c98055b1d3ca4fa790e50fb1f1b5574723
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-29 11:42:58 -1000

    moved useGeolocation and useWeatherFetch to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { AppContainer } from './App.styles';
-import CitySearch from './components/CitySearch/CitySearch';
+import React from "react";
+import { AppContainer } from "./App.styles";
+import CitySearch from "./components/CitySearch/CitySearch";
+import useGeoLocation from "./hooks/useGeolocation";
+import { useWeatherFetch } from "./hooks/useWeatherFetch";
 function App() {
+  const { location } = useGeoLocation();
+  const { weatherDaily, status, weatherHourly } = useWeatherFetch(
+    location.coordinates
+  );
   return (
     <AppContainer>
-      <CitySearch />
+      <CitySearch
+        weatherDaily={weatherDaily}
+        weatherHourly={weatherHourly}
+        coordinates={location.coordinates}
+      />
     </AppContainer>
   );
 }
