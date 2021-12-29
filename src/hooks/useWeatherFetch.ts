a0f7d3f73d542a66649a9884883af30d01353552
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-12-29 11:43:29 -1000

    created weather fetch hook

--- src/hooks/useWeatherFetch.ts
+++ src/hooks/useWeatherFetch.ts
@@ -1,1 +1,58 @@
-export {}
+import { useState, useEffect } from "react";
+import isDevModeConsoleLog from "../utils/isDevModeConsoleLog";
+export type statusType = "idle" | "searching" | "loaded" | "failed";
+export type weatherType = any;
+export type latLangUrlInput = {
+  lat: number;
+  lng: number;
+};
+const idle = "idle";
+const searching = "searching";
+const loaded = "loaded";
+const failed = "failed";
+
+const latLangURL = (coordinates: latLangUrlInput) => {
+  // Api endpoint to get weather.gov grid id
+  return `https://api.weather.gov/points/${coordinates.lat},${coordinates.lng}`;
+};
+
+export const useWeatherFetch = (coordinates: latLangUrlInput) => {
+  isDevModeConsoleLog("updating useWeatherFetch");
+  const [weatherDaily, setWeatherDaily] = useState<weatherType>([]);
+  const [status, setStatus] = useState<statusType>(idle);
+  const [weatherHourly, setWeatherHourly] = useState<weatherType>([]);
+  useEffect(() => {
+    const apiCalls = async () => {
+      if (!coordinates.lat || !coordinates.lng) {
+        return;
+      }
+      setStatus(searching);
+      const gridFetch = await fetch(latLangURL(coordinates), {});
+      if (gridFetch.status === 500) {
+        setStatus(failed);
+      }
+      if (gridFetch.ok) {
+        const gridJSON = await gridFetch.json();
+        isDevModeConsoleLog("fetching weather data");
+        const weatherFetch = await fetch(
+          `https://api.weather.gov/gridpoints/${gridJSON.properties.gridId}/${gridJSON.properties.gridX},${gridJSON.properties.gridY}/forecast`
+        );
+        const weatherFetchHourly = await fetch(
+          `https://api.weather.gov/gridpoints/${gridJSON.properties.gridId}/${gridJSON.properties.gridX},${gridJSON.properties.gridY}/forecast/hourly`
+        );
+        if (weatherFetch.status === 500) {
+          setStatus(failed);
+        }
+        if (weatherFetch.ok) {
+          const weatherJSON = await weatherFetch.json();
+          const weatherHourlyJSON = await weatherFetchHourly.json();
+          setWeatherDaily(weatherJSON.properties.periods);
+          setWeatherHourly(weatherHourlyJSON.properties.periods);
+          setStatus(loaded);
+        }
+      }
+    };
+    apiCalls();
+  }, [coordinates]);
+  return { weatherDaily, weatherHourly, status };
+};
