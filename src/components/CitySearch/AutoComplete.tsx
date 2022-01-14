47e2188ec55117563029a74ee5125b4f3820c81f
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-14 09:29:16 -1000

    created autocomplete search component

---
+++ src/components/CitySearch/AutoComplete.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+export const AutoCompleteContainer = styled.div`
+  /* max-height: 10em; */
+  width: 15em;
+  margin: 0 56px;
+  background-color: green;
+  /* overflow-y: scroll; */
+  position: fixed;
+  top: 5.75em;
+`;
+export const Option = styled.div`
+  cursor: pointer;
+  :hover {
+    background-color: blue;
+  }
+`;
+interface AutocompleteOptionsFilterTypes {
+  options: string[];
+  filter: string;
+  optionsDisplayLength: number;
+}
+const autocompleteOptionsFilter = ({
+  options,
+  filter,
+  optionsDisplayLength = 8,
+}: AutocompleteOptionsFilterTypes) => {
+  //  takes in an array of options and matches
+  //  to the filter and returns array of length
+  //  optionsDisplayLength. if the only return value matches
+  //  filter an empty array is returned
+  const displayOptions = options
+    .filter((option) => option.toLowerCase().includes(filter.toLowerCase()))
+    .slice(0, optionsDisplayLength);
+  if (displayOptions.length === 1 && displayOptions[0] === filter) {
+    return [];
+  }
+  return displayOptions;
+};
+
+interface AutoCompleteProps extends AutocompleteOptionsFilterTypes {
+  optionSelectFunction: any;
+  onClick: (e: Event) => void;
+}
+export const AutoComplete: React.FC<AutoCompleteProps> = ({
+  options,
+  filter,
+  optionsDisplayLength = 8,
+  optionSelectFunction,
+}) => {
+  return (
+    <AutoCompleteContainer>
+      {autocompleteOptionsFilter({
+        options: options,
+        filter: filter,
+        optionsDisplayLength: optionsDisplayLength,
+      }).map((x) => (
+        <Option onClick={() => optionSelectFunction(x)}>{x}</Option>
+      ))}
+    </AutoCompleteContainer>
+  );
+};
