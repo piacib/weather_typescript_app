4a03712ac9f28e34d4a0f4a29b8cb176455118c4
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-15 10:42:17 -1000

    removed useGeolocation and useWeatherfetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { AppContainer } from "./App.styles";
 import CitySearch from "./components/CitySearch/CitySearch";
-import useGeoLocation from "./hooks/useGeolocation";
-import { useWeatherFetch } from "./hooks/useWeatherFetch";
 function App() {
-  const { location } = useGeoLocation();
-  const { weatherDaily, status, weatherHourly } = useWeatherFetch(
-    location.coordinates
-  );
   return (
     <AppContainer>
-      <CitySearch
-        weatherDaily={weatherDaily}
-        weatherHourly={weatherHourly}
-        coordinates={location.coordinates}
-      />
+      <CitySearch />
     </AppContainer>
   );
 }
