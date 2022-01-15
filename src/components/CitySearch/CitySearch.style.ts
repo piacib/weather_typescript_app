9d1cba03099da789fffdc05e55ffdc379bf2c99e
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-15 10:56:01 -1000

    adjusted font size and widths

--- src/components/CitySearch/CitySearch.style.ts
+++ src/components/CitySearch/CitySearch.style.ts
@@ -1,24 +1,29 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  position: relative;
+`;
+
 export const CitySearchContainer = styled.div`
   background: white;
+  height: 2rem;
   border-radius: 40px;
   margin: 0 auto;
   padding: 10px;
-  max-width: 800px;
+  max-width: 400px;
   display: flex;
   align-items: center;
   width: 100%;
-  position: relative;
+  /* position: relative; */
 `;
 export const CitySearchInput = styled.input`
   border: none;
-  font-size: 2rem;
+  font-size: 1.3rem;
   width: 90%;
   @media (max-width: ${(props) => props.theme.mediaSizes.mobileM}) {
-    font-size: 1.5rem;
+    font-size: 1.1rem;
   }
   @media (max-width: ${(props) => props.theme.mediaSizes.mobileS}) {
-    font-size: 1.2rem;
+    font-size: 0.9rem;
   }
 `;
