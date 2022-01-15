f2c64b25eec52f459432cffa39b4b4ae1bc012a7
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-15 10:54:56 -1000

    added citySearchContainer props and values

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,11 +1,19 @@
-
 // import original module declarations
-import 'styled-components';
+import "styled-components";
 // and extend them!
-declare module 'styled-components' {
+declare module "styled-components" {
   export interface DefaultTheme {
     backgroundColor: string;
     fontColor: string;
     dataSvgSize: number;
-    }
+    mediaSizes: {
+      mobileS: string;
+      mobileM: string;
+      mobileL: string;
+    };
+    citySearchContainer: {
+      height: string;
+      padding: string;
+    };
+  }
 }
