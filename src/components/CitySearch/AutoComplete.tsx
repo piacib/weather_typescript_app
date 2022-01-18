7b16aa04b6161b98b0d2f02046fb1124f5c297ff
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-17 17:43:27 -1000

    made display responsive to rem

--- src/components/CitySearch/AutoComplete.tsx
+++ src/components/CitySearch/AutoComplete.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 export const AutoCompleteContainer = styled.div`
-  width: 15em;
-  margin: 0 56px;
-  background-color: green;
+  width: 11rem;
+  padding-bottom: 0.25rem;
+  border-radius: 0 0 12px 12px;
+  margin: 0
+    ${(props) => props.theme.citySearchContainer.widthMagnifyingGlassImage};
+  background-color: white;
+  color: black;
   position: absolute;
+
   top: calc(
     ${(props) => props.theme.citySearchContainer.height} +
       calc(${(props) => props.theme.citySearchContainer.padding} * 2)
   );
+  @media (max-width: ${(props) => props.theme.mediaSizes.mobileM}) {
+    width: 10rem;
+  }
+  @media (max-width: ${(props) => props.theme.mediaSizes.mobileS}) {
+    width: 8rem;
+  }
 `;
 export const Option = styled.div`
   cursor: pointer;
+  padding-left: 4px;
   :hover {
     background-color: blue;
   }
--- src/components/CitySearch/CitySearch.style.ts
+++ src/components/CitySearch/CitySearch.style.ts
@@ -3,13 +3,16 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: relative;
 `;
-
+export const MagnifyingGlassImage = styled.img`
+  width: ${(props) =>
+    props.theme.citySearchContainer.widthMagnifyingGlassImage}; ;
+`;
 export const CitySearchContainer = styled.div`
   background: white;
   height: 2rem;
   border-radius: 40px;
   margin: 0 auto;
-  padding: 10px;
+  padding: ${(props) => props.theme.citySearchContainer.padding};
   max-width: 400px;
   display: flex;
   align-items: center;
@@ -18,11 +21,8 @@ export const CitySearchContainer = styled.div`
 `;
 export const CitySearchInput = styled.input`
   border: none;
-  font-size: 1.3rem;
+  font-size: 1rem;
   width: 90%;
-  @media (max-width: ${(props) => props.theme.mediaSizes.mobileM}) {
-    font-size: 1.1rem;
-  }
   @media (max-width: ${(props) => props.theme.mediaSizes.mobileS}) {
     font-size: 0.9rem;
   }
