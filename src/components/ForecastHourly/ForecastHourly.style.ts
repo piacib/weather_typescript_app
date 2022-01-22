9aac5de680c4d82048498710e855f64ae6aefa63
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-21 19:52:26 -1000

    set up component for hourly weather data display

--- src/components/ForecastHourly/ForecastHourly.style.ts
+++ src/components/ForecastHourly/ForecastHourly.style.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const ForecastHourlyContainer = styled.div``
+export const ForecastHourlyContainer = styled.ul``;
+export const ForecastHourlyEntryDisplay = styled.li`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 0.5fr 0.25fr 1fr;
+  flex-direction: row;
+`;
+export const Time = styled.div``;
+export const Temperature = styled.div``;
+export const Forecast = styled.div``;
