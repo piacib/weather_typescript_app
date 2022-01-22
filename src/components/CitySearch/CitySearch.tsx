1355f5b1ddd8ee038954ecd3173c9dab6dfab790
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-22 10:08:08 -1000

    added dateDisplay state so it can be toggled removed initial example state

--- src/components/CitySearch/CitySearch.tsx
+++ src/components/CitySearch/CitySearch.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import WeatherCard from "../WeatherCard/WeatherCard";
-import { WeatherData } from "../WeatherCard/WeatherData";
 import magnifyingGlass from "./magnifying-glass.svg";
 import {
   Container,
@@ -10,10 +9,11 @@ import {
 } from "./CitySearch.style";
 import { AutoComplete } from "./AutoComplete";
 import { Coordinates } from "./CitySearch.types";
-import { useWeatherFetch } from "../../hooks/useWeatherFetch";
+import { ForecastEntry, useWeatherFetch } from "../../hooks/useWeatherFetch";
 import useGeoLocation from "../../hooks/useGeolocation";
 import Loading from "./Loading";
 import Failed from "./Failed";
+import ForecastHourly from "../ForecastHourly/ForecastHourly";
 
 type CityEntry = {
   key: number;
@@ -24,44 +24,8 @@ type CityEntry = {
 };
 const usCities: CityEntry[] = require("./usCities.json");
 const usCityName = usCities.map((entry) => entry.text);
-
-const weatherCardTestObject = {
-  dayOfTheWeek: "Sunday",
-  todaysDate: "Nov 14",
-  windSpeed: 3.7,
-  rain: 74,
-  humidity: 83,
-  pressure: 1010,
-  snow: 0,
-  temperature: 24,
-  weatherDescription: "Heavy Rain",
-  windUnits: "km/h",
-  pressureUnits: "mbar",
-};
 const defaultPlaceHolder = "Search your city";
 const locationFoundPlaceHolder = "Press search to use your location";
-
-const WeatherExampleCard = () => {
-  return (
-    <>
-      <WeatherCard
-        dayOfTheWeek={weatherCardTestObject.dayOfTheWeek}
-        todaysDate={weatherCardTestObject.todaysDate}
-        temperature={weatherCardTestObject.temperature}
-        weatherDescription={weatherCardTestObject.weatherDescription}
-      ></WeatherCard>
-      <WeatherData
-        rain={weatherCardTestObject.rain}
-        humidity={weatherCardTestObject.humidity}
-        pressure={weatherCardTestObject.pressure}
-        snow={weatherCardTestObject.snow}
-        windSpeed={weatherCardTestObject.windSpeed}
-        windUnits={weatherCardTestObject.windUnits}
-        pressureUnits={weatherCardTestObject.pressureUnits}
-      />
-    </>
-  );
-};
 const weekday = [
   "Sunday",
   "Monday",
@@ -85,11 +49,6 @@ const month = [
   "Nov",
   "Dec",
 ];
-const states = {
-  inputSelected: "inputSelected",
-  autoSelected: "autoSelected",
-  nullState: "",
-};
 const CitySearch = () => {
   const { location, status: geoLocationStatus } = useGeoLocation();
   const [city, setCity] = useState<string>("");
@@ -104,17 +63,21 @@ const CitySearch = () => {
   const [inputSelected, setInputSelected] = useState<boolean>(false);
   const { weatherDaily, status, weatherHourly } =
     useWeatherFetch(searchedLocation);
-  const date = weatherDaily[0]
-    ? new Date(weatherDaily[0].startTime)
-    : new Date();
-  console.log(status);
+  const [dateDisplayed, setDateDisplayed] = useState<ForecastEntry | null>(
+    null
+  );
+  const date = dateDisplayed ? new Date(dateDisplayed.startTime) : new Date();
+  useEffect(() => {
+    if (weatherDaily) {
+      setDateDisplayed(weatherDaily[0]);
+    }
+  }, [weatherDaily]);
   // handles autocomlete visibility
+
   useEffect(() => {
     if (autocompleteSelected) {
-      console.log("useEffect autocompleteSelected");
       setAutocompleteVisible(false);
     } else if (inputSelected) {
-      console.log("useEffect inputSelcted");
       setAutocompleteVisible(true);
     }
     if (!autocompleteSelected && !inputSelected) {
@@ -123,21 +86,19 @@ const CitySearch = () => {
   }, [autocompleteSelected, inputSelected]);
   // handles setting search location coords
   useEffect(() => {
-    console.log("checking city");
     const result = usCities.find((usCity) => usCity.text === city);
     if (result) {
-      console.log("searching input");
       setSearchedLocation({ lat: result.latitude, lng: result.longitude });
     } else if (geoLocationStatus === "loaded") {
       if (
         location.lat !== searchedLocation.lat &&
         location.lng !== searchedLocation.lng
       ) {
-        console.log("searching geo");
         setSearchedLocation(location);
       }
     }
   }, [city, geoLocationStatus, location]);
+  console.log(weatherDaily);
   console.log("_____");
   return (
     <Container>
@@ -164,7 +125,7 @@ const CitySearch = () => {
           }
           type="text"
         />
-        {autocompleteVisible ? (
+        {autocompleteVisible && (
           <AutoComplete
             onClick={(e) => {
               console.log("autocomplete selected");
@@ -178,23 +139,22 @@ const CitySearch = () => {
               setAutocompleteSelected(true);
             }}
           />
-        ) : null}
+        )}
       </CitySearchContainer>
-
       {status === "searching" && <Loading />}
-      {status === "loaded" ? (
-        weatherDaily && weatherDaily[0] ? (
+      {status === "failed" && <Failed />}
+      {status === "loaded" && dateDisplayed && (
+        <>
           <WeatherCard
             dayOfTheWeek={weekday[date.getDay()]}
             todaysDate={`${month[date.getMonth()]} ${date.getDate()}`}
-            temperature={weatherDaily[0].temperature}
-            weatherDescription={weatherDaily[0].shortForecast}
+            temperature={dateDisplayed.temperature}
+            iconSrc={dateDisplayed.icon}
+            weatherDescription={dateDisplayed.shortForecast}
           />
-        ) : (
-          <WeatherExampleCard />
-        )
-      ) : null}
-      {status === "failed" && <Failed />}
+          <ForecastHourly forecastHourlyArray={weatherHourly} />
+        </>
+      )}
     </Container>
   );
 };
