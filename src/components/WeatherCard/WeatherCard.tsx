c4a68051219489ad8e70b7474a5357a75b38c064
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-25 10:08:07 -1000

    formatted

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,42 +1,47 @@
-import React from 'react'
-import SvgSprite from '../../utils/SvgSpriteLoader'
+import React from "react";
+import SvgSprite from "../../utils/SvgSpriteLoader";
 import {
-    WeatherCardContainer,
-    Date,
-    WeekDate,
-    Temperature,
-    DateContainer,
-    WhiteLine,
-    WeatherDescription
-} from './WeatherCard.style'
-import { WeatherData } from './WeatherData'
+  WeatherCardContainer,
+  Date,
+  WeekDate,
+  Temperature,
+  DateContainer,
+  WhiteLine,
+  WeatherDescription,
+} from "./WeatherCard.style";
+import { WeatherData } from "./WeatherData";
 
 interface WeatherDisplayProps {
-    dayOfTheWeek: string;
-    todaysDate: string;
-    temperature: number;
-    weatherDescription: string;
+  dayOfTheWeek: string;
+  todaysDate: string;
+  temperature: number;
+  weatherDescription: string;
+  iconSrc: string;
 }
 
-const WeatherCard:React.FC<WeatherDisplayProps> = ({
-    dayOfTheWeek,
-    todaysDate,
-    temperature,
-    weatherDescription 
+const WeatherCard: React.FC<WeatherDisplayProps> = ({
+  dayOfTheWeek,
+  todaysDate,
+  temperature,
+  weatherDescription,
+  iconSrc,
 }) => {
-    return (
-        <WeatherCardContainer>
-            <SvgSprite type='static' image='rainy-1'/>
-            <DateContainer>
-            <Date>{dayOfTheWeek}</Date>
-            <WhiteLine/>
-            <WeekDate>{todaysDate}</WeekDate>
-            </DateContainer>
-            <Temperature>{temperature}<span>&#176;</span></Temperature>
-            <WeatherDescription>{weatherDescription}</WeatherDescription>
-            
-        </WeatherCardContainer>
-    )
-}
+  return (
+    <WeatherCardContainer>
+      <img src={iconSrc} alt={"weather"} />
+      {/* <SvgSprite type="static" image="rainy-1" /> */}
+      <DateContainer>
+        <Date>{dayOfTheWeek}</Date>
+        <WhiteLine />
+        <WeekDate>{todaysDate}</WeekDate>
+      </DateContainer>
+      <Temperature>
+        {temperature}
+        <span>&#176;</span>
+      </Temperature>
+      <WeatherDescription>{weatherDescription}</WeatherDescription>
+    </WeatherCardContainer>
+  );
+};
 
-export default WeatherCard
+export default WeatherCard;
