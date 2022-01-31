487a7c8a7b341faa47658186e53a819808926abd
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-01-31 11:54:23 -1000

    moved styles to seperate file

---
+++ src/components/CitySearch/WeatherDateToggle.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+// WeatherDateToggle components
+export const WeatherDateToggle = styled.select`
+  width: 100px;
+  margin: 10px auto;
+  border-radius: 40px;
+  padding: 8px;
+`;
+export const DateToggleButton = styled.option``;
