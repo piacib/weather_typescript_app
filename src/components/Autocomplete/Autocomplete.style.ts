70ed0af3ce9ecacf0f28c079d34a5febf4147a71
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-02-01 18:07:37 -1000

    added styles for new components removed unused styles

--- src/components/Autocomplete/Autocomplete.style.ts
+++ src/components/Autocomplete/Autocomplete.style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-export const AutoCompleteContainer = styled.div`
+export const AutoCompleteContainer = styled.select`
   width: 11rem;
   padding-bottom: 0.25rem;
   border-radius: 0 0 12px 12px;
@@ -13,17 +13,24 @@ export const AutoCompleteContainer = styled.div`
     ${(props) => props.theme.citySearchContainer.height} +
       calc(${(props) => props.theme.citySearchContainer.padding} * 2)
   );
-  @media (max-width: ${(props) => props.theme.mediaSizes.mobileM}) {
-    width: 10rem;
-  }
-  @media (max-width: ${(props) => props.theme.mediaSizes.mobileS}) {
-    width: 8rem;
-  }
 `;
-export const Option = styled.div`
+export const Option = styled.option`
   cursor: pointer;
   padding-left: 4px;
   :hover {
     background-color: blue;
   }
 `;
+export const AutocompleteForm = styled.form`
+  @media (max-width: ${(props) => props.theme.mediaSizes.mobileM}) {
+    width: 10rem;
+  }
+  @media (max-width: ${(props) => props.theme.mediaSizes.mobileS}) {
+    width: 7rem;
+  }
+`;
+export const AutocompleteInput = styled.input`
+  border: none;
+`;
+export const AutocompleteList = styled.datalist``;
+export const AutocompleteOption = styled.option``;
